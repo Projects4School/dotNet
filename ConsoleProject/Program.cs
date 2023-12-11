ea5ef928da7c8d1a5e57aba26286f3e76a0b44ac
Gaspar M <gaspar.musitelli@ecole-isitech.fr>
2023-12-11 14:41:51 +0100

    Init git

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static int Sum(IEnumerable<object> values)
+    {
+        var sum = 0;
+        foreach (var item in values)
+        {
+            switch(item)
+            {
+                case 0:
+                    break;
+                case int val:
+                    sum += val;
+                    break;
+                case IEnumerable<object> subList when subList.Any():
+                    sum += Sum(subList);
+                    break;
+            }
+        }
+        return sum;
+    }
+}
