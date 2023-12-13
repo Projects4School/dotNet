e034ae33e16c9f65126217baaad8f4abcb09faf2
Gaspar M <gaspar.musitelli@ecole-isitech.fr>
2023-12-13 09:34:35 +0100

    Add CRUD for Book

--- BookStoreAPI/Controllers/BookController.cs
+++ BookStoreAPI/Controllers/BookController.cs
@@ -17,47 +17,117 @@ public class BookController : ControllerBase
         _dbContext = dbContext;
     }
 
-    // Ceci est une annotation, elle permet de définir des métadonnées sur une méthode
-    // ActionResult designe le type de retour de la méthode de controller d'api
     [HttpGet]
-    public ActionResult<List<Book>> GetBooks()
+    public async Task<ActionResult<List<Book>>> GetBooks()
     {
-
-        var books = new List<Book>
-        {
-            new() { Id = 1, Title = "Le seigneur des anneaux", Author = "J.R.R Tolkien" }
-        };
-
-        return Ok(books);
-
+        return await _dbContext.Books.ToListAsync();
     }
-    // POST: api/Book
-    // BODY: Book (JSON)
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(200, Type = typeof(Book))]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<Book>> GetBook(int id)
+    {
+        Book? existingBook = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        if (existingBook is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            return Ok(existingBook);
+        }
+    }
+
     [HttpPost]
     [ProducesResponseType(201, Type = typeof(Book))]
     [ProducesResponseType(400)]
     public async Task<ActionResult<Book>> PostBook([FromBody] Book book)
     {
-        // we check if the parameter is null
-        if (book == null)
+        if (book is null)
         {
             return BadRequest();
         }
-        // we check if the book already exists
         Book? addedBook = await _dbContext.Books.FirstOrDefaultAsync(b => b.Title == book.Title);
-        if (addedBook != null)
+        if (addedBook is not null)
         {
             return BadRequest("Book already exists");
         }
         else
         {
-            // we add the book to the database
-            await _dbContext.Books.AddAsync(book);
-            await _dbContext.SaveChangesAsync();
+            try
+            {
+                await _dbContext.Books.AddAsync(book);
+                await _dbContext.SaveChangesAsync();
+                return Created("api/book", book);
+            }
+            catch (DbUpdateException exception)
+            {
+                return BadRequest(exception.InnerException?.Message);
+            }
+        }
+    }
 
-            // we return the book
-            return Created("api/book", book);
+    [HttpPut("{id}")]
+    [ProducesResponseType(200, Type = typeof(Book))]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<Book>> PutBook(int id, [FromBody] Book book)
+    {
+        // we check if the parameter is null
+        if (book is null || id != book.Id)
+        {
+            return BadRequest();
+        }
+        // we check if the book already exists
+        Book? existingBook = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        if (existingBook is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            existingBook = new Book
+            {
+                Title = book.Title,
+                Author = book.Author,
+                Abstract = book.Abstract
+            };
 
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                return Ok(existingBook);
+            }
+            catch (DbUpdateException exception)
+            {
+                return BadRequest(exception.InnerException?.Message);
+            }
+        }
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(200, Type = typeof(Book))]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<Book>> DeleteBook(int id)
+    {
+        Book? existingBook = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        if (existingBook is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            try
+            {
+                _dbContext.Books.Remove(existingBook);
+                await _dbContext.SaveChangesAsync();
+                return Ok(existingBook);
+            }
+            catch (DbUpdateException exception)
+            {
+                return BadRequest(exception.InnerException?.Message);
+            }
         }
     }
 }
